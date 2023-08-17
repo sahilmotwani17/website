e2c5785bc5d5b9b2b1cc088d5b0e02e056188ab6
sahilmotwani17 <smotwani17@gmail.com>
2023-08-17 11:03:51 +0530

    contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Form, Button, Row, Col } from 'react-bootstrap'; // Import React Bootstrap components
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import NavigationBar from './Navbar';
 
 const ContactForm = () => {
@@ -9,6 +9,9 @@ const ContactForm = () => {
     email: '',
     number: '',
   });
+
+  const [showForm, setShowForm] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -21,13 +24,11 @@ const ContactForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // Send the form data to the backend server
       const response = await axios.post('http://127.0.0.1:8000/api/contact', formData, {
         headers: { 'Content-Type': 'application/json' },
       });
-      console.log(response.data); // Server response number
+      console.log(response.data);
 
-      // Clear input fields after successful submission
       setFormData({
         name: '',
         email: '',
@@ -35,60 +36,87 @@ const ContactForm = () => {
       });
     } catch (error) {
       console.error('Error submitting form:', error);
-      // Handle error scenarios, such as displaying an error message to the user.
     }
   };
 
+  const handleJoinTeamClick = () => {
+    setShowForm(false);
+    setShowMessage(true);
+  };
+
+  const handleConnectClick = () => {
+    setShowMessage(false);
+    setShowForm(true);
+  };
+
   return (
-    <div id="contact" style={{ height: 350 }}>
+    <div id="contact" style={{ height: '39.86vw' }}>
       <h1>Contact</h1>
       <NavigationBar />
-      <Form onSubmit={handleSubmit} style={{ margin: 20 }}>
-        <Row>
-          <Col md={3}>
-            <Form.Group controlId="formName">
-              <Form.Label>Full name</Form.Label>
-              <Form.Control
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-                className="custom-form-group"
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formNumber">
-              <Form.Label>Contact number</Form.Label>
-              <Form.Control
-                as="textarea"
-                name="number"
-                value={formData.number}
-                onChange={handleChange}
-                rows={1}
-                required
-                className="custom-form-group"
-              />
-            </Form.Group>
-          </Col>
-          <Col md={3}>
-            <Form.Group controlId="formEmail">
-              <Form.Label>E-mail</Form.Label>
-              <Form.Control
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-                className="custom-form-group"
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Button id='contactsubmit' variant='outline-warning' type="submit" style={{ marginTop: '10px' }}>
-          Submit
+      <div style={{ display: 'flex', justifyContent: 'center',fontFamily:'Proxima Nova' ,width: '28.48vw', height: '2.9vw', marginTop: '2.3vw',marginLeft:'2vw' ,borderRadius: '0.72vw', border: '0.07vw solid rgba(255, 203, 111, 1)' }}>
+        <Button variant="primary" onClick={handleJoinTeamClick} style={{ backgroundColor: showMessage ? 'rgba(255, 203, 111, 1)' : 'transparent', color: showMessage ? 'white' : 'black', border: 'none', outline: 'none',fontSize:'1.3vw',marginRight: '2vw',paddingTop: '0.5vw', paddingBottom: '0.5vw'  }}>
+          Join Our Team
         </Button>
-      </Form>
+        <Button variant="success" onClick={handleConnectClick} style={{ backgroundColor: showForm ? 'rgba(255, 203, 111, 1)' : 'transparent', color: showForm ? 'white' : 'black', border: 'none', outline: 'none',fontSize:'1.3vw',paddingTop: '0.5vw', paddingBottom: '0.5vw'  }}>
+          Connect With Us
+        </Button>
+      </div>
+      
+      {showForm && (
+        <Form onSubmit={handleSubmit} style={{marginTop:'1.45vw'}} >
+          <span style={{fontFamily:'Proxima Nova', fontSize:'1.3vw'}}>Give us your details in case you have any queries and we will contact you at the earliest. Let’s talk.</span>
+          <br/>
+          <span style={{fontFamily:'Proxima Nova', fontSize:'1.01vw', color:'rgba(158, 158, 158, 1)'}}>Mandatory fields are marked with *</span>
+          <Row>
+            <Col md={3}>
+              <Form.Group controlId="formName" style={{ marginTop: '2.5vw' }} >
+                <Form.Label>Full name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                  className="custom-form-group transparent-input"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formNumber">
+                <Form.Label>Contact number</Form.Label>
+                <Form.Control
+                  variant="text"
+                  name="number"
+                  value={formData.number}
+                  onChange={handleChange}
+                  rows={1}
+                  required
+                  className="custom-form-group transparent-input"
+                  style={{ height: '2.75vw' }}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={3}>
+              <Form.Group controlId="formEmail" style={{ marginTop: '2.5vw' }}>
+                <Form.Label>E-mail</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  className="custom-form-group transparent-input"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Button id="contactsubmit" variant="outline-warning" type="submit" style={{ marginTop: '0.72vw' }}>
+            Submit
+          </Button>
+        </Form>
+      )}
+      {showMessage && (
+        <p>We are not hiring at the moment.</p>
+      )}
     </div>
   );
 };
